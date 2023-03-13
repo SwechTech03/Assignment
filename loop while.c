f33c8959605043fadcab86b2a299ebfd90e56aeb
SwechTech03 <127714830+SwechTech03@users.noreply.github.com>
2023-03-13 11:14:04 +0545

    while loop in c has been uploaded
    this is just start of beginning

---
+++ loop while.c
@@ -0,0 +1,28 @@
+// 2,4,6....19th term
+#include<stdio.h>
+#include<math.h>
+   int main()
+
+   {
+       int i,j;
+       i=2;
+       j=1;
+       while(j<=19)
+       {
+           printf("%d , ",i);
+           i=i+2;
+
+           j=j+1;
+       }
+
+
+
+
+
+
+
+
+       return 0;
+
+
+   }
